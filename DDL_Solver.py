1c6a896d5ce53c95e8f90c19d8904dfda108d8df
jtennies10 <jdtennies10@gmail.com>
2018-11-20 19:10:59 -0500

    basic class stubs

---
+++ DDL_Solver.py
@@ -0,0 +1,14 @@
+
+
+
+"""Defines the Truck class, which is instantiated for each
+   of the given trucks used by DLD"""
+class Truck:
+
+
+"""Defines the Delivery class, which is instantieated for 
+    each delivery in the deliveries file"""
+class Delivery:
+
+
+
